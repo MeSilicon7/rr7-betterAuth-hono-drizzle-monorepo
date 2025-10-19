9a8ce02be778f4d4578d8429bf320dd90b19dcf3
MeSilicon7 <github.gpynn@passmail.net>
2025-10-19 14:51:09 +0600

    fix: drizzle config

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'drizzle-kit';
-import { DATABASE_URL } from './src/env';
+import 'dotenv/config';
 
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
 });
